9b5a20906b712de28197dc167e6d62232c643014
lucasmdo <lucas.m.de.oliveira@accenture.com>
2024-06-12 10:31:50 -0300

    feat: auth scaffolding

---
+++ packages/auth/src/bootstrap.js
@@ -0,0 +1,38 @@
+console.log("Marketing bootstrap");
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
+import App from "./App";
+
+// Mount function to start up the app
+const mountApp = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen((location) => {
+      console.log("Navigated to", location.pathname);
+      onNavigate(location.pathname);
+    });
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate: (path) => {
+      const { pathname: currentPath } = history.location;
+      if (currentPath !== path) {
+        history.push(path);
+      }
+    },
+  };
+};
+
+// If we are in development and in isolation mode, we need to mount the app
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_auth-dev-root");
+  if (devRoot) mountApp(devRoot, { defaultHistory: createBrowserHistory() });
+  else console.warn("Could not find the dev root");
+}
+
+export { mountApp };
